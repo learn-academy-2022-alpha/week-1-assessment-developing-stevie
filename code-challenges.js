c9c902d467c9ad7d320d81e511dd773f4d056660
developing-stevie <mitchellevie@icloud.com>
2022-02-22 12:50:52 -0500

    stevies-done-assessment

--- code-challenges.js
+++ code-challenges.js
@@ -22,21 +22,57 @@ const temp2 = 350
 const temp3 = 212
 
 
+
+
+
+
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+//console.log(myNumbers1.concat(myNumbers2).length)
+//log and combine myNumbers1 & myNumbers2--> 
+//output -->[[3, 7, 0, 36, -9, 8, -7, 42, 9, 13
+//.length counts the number of indexes -->
+//prints the length of the array instead of the output above making
+//output -->10
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
+// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ahplA"
 
 const currentCohort = "Alpha 2022"
 
+//console.log(currentCohort.split("").reverse().join(""));
+
+//logs value of currentCohort >
+//"splits" the values of the string into a index of characters
+//"reverses" the order of the string index>
+//"joins" them back together into a reversed string>
+//output-->"1202 ahplA"
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
 const myArray = [13, 34, 5, 10, 27, 42]
 
+
+
+for(let i = 0; i < myArray.length; i++){
+    if(myArray.indexOf(i) % 2 !== 0){
+      console.log("odd")
+    } else {
+      console.log("even")
+    }
+  }
+
+// currently working the way i want to but not getting the correct math
+//let i equal zero and is 0
+//if i is greater than myArray and its length
+//increment the value of i
+//i returns the original value
+//----> output is wrong but prints (odd,odd,odd,odd,odd,even)
+
+
+  
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
@@ -47,3 +83,6 @@ const number2 = 100
 // Set two:
 const number3 = 27
 const number4 = 24
+
+
+
